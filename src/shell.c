17b85ee9b0829d69a9eb8ba0362a2a8ade185d5b
Raph <ran.demoulin@gmail.com>
2023-03-02 10:53:23 +0100

    fausse_modif

--- src/shell.c
+++ src/shell.c
@@ -12,7 +12,7 @@ int main() {
     while (1) {
         struct cmdline *l;
         int i, new_in, new_out;
-        char **cmd = l->seq[i];
+        char **cmd;
         int is_pipe = 0;
         int* mat[100]; //on va stocker les pipes dans un tableau de 100 pipes
 
@@ -32,7 +32,7 @@ int main() {
             continue;
         }
 
-        if (!strcmp(cmd[0], "quit")) {
+        if (!strcmp(l->seq[0][0], "quit")) {
             printf("exit\n");
             exit(0);
         }
@@ -44,30 +44,30 @@ int main() {
                 fprintf(stderr, "Error: %s: %s\n", l->in, strerror(errno));
                 exit(1);
             }
-            Dup2(new_in, 0);
+            dup2(new_in, 0);
         }
         if (l->out) {
-            new_out = Open(l->out, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+            new_out = open(l->out, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
             if (new_out == -1) {
                 fprintf(stderr, "Error: %s: %s\n", l->out, strerror(errno));
                 exit(1);
             }
-            Dup2(new_out, 1);
+            dup2(new_out, 1);
         }
 
 
 
 
-        for (i = 0; l->seq[i] != 0; i++) {
-
+        for (i = 0; l->seq[i] != NULL; i++) {
+            cmd=l->seq[i];
             if (!strcmp(cmd[0], "quit")) {
                 printf("exit\n");
                 is_pipe = 1;
             }
 
-            if (i==0 && l->seq[i+1]==0)                                             //si c'est le premier element de la sequence et le dernier
+            if (i==0 && l->seq[i+1]==NULL)                                             //si c'est le premier element de la sequence et le dernier
             {
-                if (Fork() == 0) { //on cree un fils qui va executer la commande
+                if (fork() == 0) { //on cree un fils qui va executer la commande
                     if (execvp(cmd[0], cmd) == -1) {
                         fprintf(stderr, "Error: %s: %s\n", cmd[0], strerror(errno));
                         exit(1);
@@ -78,16 +78,16 @@ int main() {
             }
 
 
-            else if (l->seq[i+1]!=0)                                        //si c'est pas le dernier
+            else if (l->seq[i+1]!=NULL)                                        //si c'est pas le dernier
             {
                 is_pipe = 1;
                 int fd[2];
                 pipe(fd);
                 mat[i] = fd;
-                if (Fork() == 0) { //on cree un fils qui va executer la commande
-                    Dup2(mat[i][1], 1); //on redirige la sortie standard vers le pipe
+                if (fork() == 0) { //on cree un fils qui va executer la commande
+                    dup2(mat[i][1], 1); //on redirige la sortie standard vers le pipe
                     if (i!=0) {
-                        Dup2(mat[i-1][0], 0); //on redirige l'entree standard vers le pipe
+                        dup2(mat[i-1][0], 0); //on redirige l'entree standard vers le pipe
                     }
                     if (execvp(cmd[i], cmd) == -1) {
                         fprintf(stderr, "Error: %s: %s\n", cmd[i], strerror(errno));
@@ -105,8 +105,8 @@ int main() {
                 int fd[2];
                 pipe(fd);
                 mat[i] = fd;
-                if (Fork() == 0) { //on cree un fils qui va executer la commande
-                    Dup2(mat[i-1][0], 0); //on redirige l'entree standard vers le pipe
+                if (fork() == 0) { //on cree un fils qui va executer la commande
+                    dup2(mat[i-1][0], 0); //on redirige l'entree standard vers le pipe
                     if (execvp(cmd[0], cmd) == -1) {
                         fprintf(stderr, "Error: %s: %s\n", cmd[0], strerror(errno));
                         exit(1);
@@ -118,9 +118,9 @@ int main() {
         }
         if (is_pipe != 0){
         //on ferme tout les pipes
-            for (i = 0; l->seq[i] != 0; i++) {
-                Close(mat[i][0]);
-                Close(mat[i][1]);
+            for (i = 0; l->seq[i] != NULL; i++) {
+                close(mat[i][0]);
+                close(mat[i][1]);
             }
         }
     }
