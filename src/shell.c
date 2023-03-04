bff3ba19f14dfb7c27a3ec73d5a8202f967a25dd
4ppolyon <skyle.omg03@gmail.com>
2023-03-04 21:40:10 +0100

    cas 1 seul pipe

--- src/shell.c
+++ src/shell.c
@@ -19,36 +19,43 @@ void Aucun_pipe(char **cmd, int new_in, int new_out){
     }
 }
 
-void Debut_Milieu(int i,char **cmd,int** MatPipe, int new_in){
-    pipe(MatPipe[i]);
-    printf("indice %d, lecture %d, ecriture %d\n",i,MatPipe[i][0],MatPipe[i][1]);
-    if (Fork() == 0) { //on cree un fils qui va executer la commande
-        Dup2(MatPipe[i][1], 1); //on redirige la sortie standard vers l'entré du pipe
-        Close(MatPipe[i][0]); //on ferme la lecture du pipe
-        if (i!=0) {
-            Dup2(MatPipe[i-1][0], 0); //on redirige l'entree standard vers la sortie du pipe
-        }else{
-            if(new_in){
-                dup2(new_in, 0);
-            }
+void Debut_Milieu_Fin( struct cmdline *l,int** MatPipe, int new_in, int new_out){
+    pipe(MatPipe[0]);
+    if (Fork() == 0) {
+        Dup2(MatPipe[0][1], 1);
+        Close(MatPipe[0][0]);
+        if(new_in){
+            dup2(new_in, 0);
         }
-        if (execvp(cmd[0], cmd) == -1) {
-            fprintf(stderr, "Error: %s: %s\n", cmd[i], strerror(errno));
+        if (execvp(l->seq[0][0], l->seq[0]) == -1) {
+            fprintf(stderr, "Error: %s: %s\n", l->seq[0][0], strerror(errno));
             exit(1);
         }
     }
-    Close(MatPipe[i][1]); //on ferme l'ecriture du pipe
-}
+    Close(MatPipe[0][1]);
 
-void Fin(int i,char **cmd,int** MatPipe, int new_out){ //
-    printf("indice %d, lecture %d, ecriture %d\n",i,MatPipe[i-1][0],MatPipe[i-1][1]);
+    if (!strcmp(l->seq[1][0], "quit")) {
+        printf("exit\n");
+        while (wait(NULL) > 0);
+        //on ferme tout les pipes
+        for (int i = 0; l->seq[i + 1] != NULL; i++) {
+            // ici il y a un probleme de fermeture des pipes (pour les redirections probablement)
+            Close(MatPipe[i][0]);
+        }
+        //on libere la memoire
+        for (int i = 0; l->seq[i] != NULL; i++) {
+            free(MatPipe[i]);
+        }
+        free(MatPipe);
+        exit(0);
+    }
     if (fork() == 0) { //on cree un fils qui va executer la commande
         if(new_out){
             Dup2(new_out, 1);
         }
-        Dup2(MatPipe[i-1][0], 0); //on redirige l'entree standard vers la sortie du pipe
-        if (execvp(cmd[0], cmd) == -1) {
-            fprintf(stderr, "Error: %s: %s\n", cmd[0], strerror(errno));
+        Dup2(MatPipe[0][0], 0); //on redirige l'entree standard vers la sortie du pipe
+        if (execvp(l->seq[1][0], l->seq[1]) == -1) {
+            fprintf(stderr, "Error: %s: %s\n", l->seq[1][0], strerror(errno));
             exit(1);
         }
     }
@@ -57,22 +64,11 @@ void Fin(int i,char **cmd,int** MatPipe, int new_out){ //
 int main() {
     while (1) {
         struct cmdline *l;
-        int i, j, new_in = 0, new_out = 0;
-        char **cmd;
+        int j, new_in = 0, new_out = 0;
         int is_pipe = 0;
         int **MatPipe; //on va stocker les pipes dans un tableau
         printf("shell> ");
         l = readcmd();
-        for (j = 0; l->seq[j] != NULL; j++) { //on compte le nombre de commandes
-        }
-        if (j > 1) {
-            is_pipe = 1;
-            // allocation de la memoire pour le tableau de pipes
-            MatPipe = malloc(j*sizeof(int*));
-            for (i = 0; i < j; i++) {
-                MatPipe[i] = malloc(2*sizeof(int));
-            }
-        }
         /* If input stream closed, normal termination */
         if (!l) {
             printf("exit\n");
@@ -83,57 +79,56 @@ int main() {
             printf("error: %s\n", l->err);
             continue;
         }
-        if (!strcmp(l->seq[0][0], "quit")) {
-            printf("exit\n");
-            exit(0);
-        }
-        if (l->in) {
-            new_in = open(l->in, O_RDONLY);
-            if (new_in == -1) {
-                fprintf(stderr, "Error: %s: %s\n", l->in, strerror(errno));
-                exit(1);
+        if (l->seq[0]) {
+            for (j = 0; l->seq[j] != NULL; j++) { //on compte le nombre de commandes
             }
-        }
-        if (l->out) {
-            new_out = open(l->out, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
-            if (new_out == -1) {
-                fprintf(stderr, "Error: %s: %s\n", l->out, strerror(errno));
-                exit(1);
-            }
-        }
-        for (i = 0; l->seq[i] != NULL; i++) {
-            cmd=l->seq[i];
-            if (!strcmp(cmd[0], "quit")) {
-                printf("exit\n");
+            if (j == 2) {
                 is_pipe = 1;
+                // allocation de la memoire pour le tableau de pipes
+                MatPipe = malloc(sizeof(int*));
+                MatPipe[0] = malloc(2*sizeof(int));
+            } else if (j > 2) {
+                fprintf(stderr,"il y a plus qu'un seul pipe\n");
+                exit(1);
             }
-
-            if (i==0 && l->seq[i+1]==NULL){                 //si c'est le premier element de la sequence et le dernier
-                Aucun_pipe(cmd, new_in, new_out);
+            if (!strcmp(l->seq[0][0], "quit")) {
+                printf("exit\n");
+                exit(0);
             }
-            
-            else if (l->seq[i+1]!=NULL){                   //si c'est pas le dernier
-                Debut_Milieu(i,cmd,MatPipe,new_in);
+            if (l->in) {
+                new_in = open(l->in, O_RDONLY);
+                if (new_in == -1) {
+                    fprintf(stderr, "Error: %s: %s\n", l->in, strerror(errno));
+                    exit(1);
+                }
             }
-            
-            else{                                          //c'est une fin
-                Fin(i,cmd,MatPipe, new_out);
+            if (l->out) {
+                new_out = open(l->out, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+                if (new_out == -1) {
+                    fprintf(stderr, "Error: %s: %s\n", l->out, strerror(errno));
+                    exit(1);
+                }
             }
-        }
-        if (is_pipe != 0){
-        //on ferme tout les pipes
-            for (i = 0; l->seq[i+1] != NULL; i++) {
-                // ici il y a un probleme de fermeture des pipes (pour les redirections probablement)
-                Close(MatPipe[i][0]);
+            if (j == 1) {
+                Aucun_pipe(l->seq[0], new_in, new_out);
+            } else {
+                Debut_Milieu_Fin(l, MatPipe, new_in, new_out);
             }
-        }
-        while (wait(NULL) > 0);
-        //on libere la memoire
-        if (is_pipe != 0){
-            for (i = 0; l->seq[i] != NULL; i++) {
-                free(MatPipe[i]);
+            while (wait(NULL) > 0);
+            if (is_pipe != 0) {
+                //on ferme tout les pipes
+                for (int i = 0; l->seq[i + 1] != NULL; i++) {
+                    // ici il y a un probleme de fermeture des pipes (pour les redirections probablement)
+                    Close(MatPipe[i][0]);
+                }
             }
-            free(MatPipe);
+            //on libere la memoire
+            if (is_pipe != 0) {
+                for (int i = 0; l->seq[i] != NULL; i++) {
+                    free(MatPipe[i]);
+                }
+                free(MatPipe);
+            }
         }
     }
 }
