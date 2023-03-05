8577efa0ce11e0c7ae760e382daf733c93aeb980
4ppolyon <78137075+4ppolyon@users.noreply.github.com>
2023-03-05 19:17:51 +0100

    Update shell.c
    add a tab

--- src/shell.c
+++ src/shell.c
@@ -28,16 +28,16 @@ int main()
 			continue;
 		}
 
-        char **cmd = l->seq[0]; //on recupere la commande et ses arguments vu que l'on a pas de pipe
-        if(!strcmp(cmd[0],"quit")){ //si la commande est quit on quitte le shell
-            exit(0);
-        }
-        if(fork()==0){ //on cree un fils pour executer la commande
-            if(execvp(cmd[0],cmd) == -1){ //si la commande n'existe pas on affiche un message d'erreur
-                printf("Commande Inconnue\n");
-            }
-        }else{
-            Wait(NULL); //on attend la fin de l'execution de la commande
-        }
+		char **cmd = l->seq[0]; //on recupere la commande et ses arguments vu que l'on a pas de pipe
+		if(!strcmp(cmd[0],"quit")){ //si la commande est quit on quitte le shell
+		    exit(0);
+		}
+		if(fork()==0){ //on cree un fils pour executer la commande
+		    if(execvp(cmd[0],cmd) == -1){ //si la commande n'existe pas on affiche un message d'erreur
+			printf("Commande Inconnue\n");
+		    }
+		}else{
+		    Wait(NULL); //on attend la fin de l'execution de la commande
+		}
 	}
 }
